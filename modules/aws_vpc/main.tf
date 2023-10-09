40db919d164bd45d7c6e5ef86553bc0eed59e9aa
anurodhacharya <anurodh.acharya10@gmail.com>
2023-10-08 17:06:04 -0700

    Added variables

--- modules/aws_vpc/main.tf
+++ modules/aws_vpc/main.tf
@@ -1,9 +1,19 @@
 # VPC
 resource "aws_vpc" "prod-vpc" {
-  cidr_block       = "172.16.0.0/16"
+  cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
 
-  tags = {
-    Name = "prod-vpc"
+  tags = var.vpc_tag
+}
+
+variable "vpc_cidr" {
+  type = string
+  default = "172.16.0.0/16"
+}
+
+variable "vpc_tag" {
+  type = map(any)
+  default = {
+    "Name" = "prod-vpc"
   }
 }
